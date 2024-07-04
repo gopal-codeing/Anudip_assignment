7c4eb64d65c31ddc2433fcb5cc7188861bc61677
Gopal Ji <143545132+gopal-codeing@users.noreply.github.com>
2024-07-05 01:06:48 +0530

    Maximumnumber.java

---
+++ Maximumnumber.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Maximumnumber{
+   public static void main(String... org){
+       Scanner obj = new Sacnner(System.in);
+     int num1 = obj.nextInt();
+     int num2 = obj.nextInt();
+     System.out.println(num1 > num2 ? num1:num2);
+   }
+}

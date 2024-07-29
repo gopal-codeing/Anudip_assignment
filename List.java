ca18bb5d3f7ca4eae36d2cf0522d29e0bb5ccccc
Gopal Ji <143545132+gopal-codeing@users.noreply.github.com>
2024-07-29 17:25:36 +0530

    List.java

---
+++ List.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class List{
+     public static void main(String []args){
+        List obj = new ArrayList(); // List can store any type of data;
+        ArrayList obj = new ArrayList(); // boath are same 
+        obj.add("hello");
+        obj.add(1);
+        obj.add(1.0f);
+         obj.add('f');
+        System.out.print(obj);  
+     }
+}

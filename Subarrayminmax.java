420ce4459c32bc5e04037b8134a2e7b3561ac0c2
Gopal Ji <143545132+gopal-codeing@users.noreply.github.com>
2024-08-03 09:43:56 +0530

    Subarrayminmax.java

--- Subarrayminmax.java
+++ Subarrayminmax.java
@@ -1,20 +1,24 @@
 class Subarrayminmax{
     public static void main(String yo[]){
-        int arr[] ={2,4,5, 3, 6, 7, 9, 4, 5, 6};
-      int max = arr[2];
-        for(int i= 2;i <=6 ;i++){
+        int arr[] = {2,4,5, 3, 6, 7, 9, 4, 5, 6};
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Entre the number two size between 0 to arr of size :");
+        int num1 =  sc.nextInt();
+        int num2 =  sc.nextInt();
+        int max = arr[num1];
+        for(int i= num1; i <=num2; i++) {
             if(arr[i] > max)
-              max = arr[i];
+                max = arr[i];
         }
-        int secondmin = arr[2];
-        for(int i=2;i<=6;i++){
+        int secondmin = arr[num1];
+        for(int i=num1; i<=num2; i++) {
             if(arr[i] < max && arr[i] > secondmin )
-              secondmin = arr[i];
+                secondmin = arr[i];
         }
-        for(int i=2;i<=6;i++)
+        for(int i=num1; i<=num2; i++)
             System.out.print(arr[i] + " ");
-        
-       System.out.println();
+
+        System.out.println();
         System.out.print("Second max : "+secondmin);
     }
 }

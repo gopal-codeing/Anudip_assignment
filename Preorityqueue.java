2822134290da3b95ded9195b925fdd35e3871d4c
Gopal Ji <143545132+gopal-codeing@users.noreply.github.com>
2024-09-04 15:45:04 +0530

    Preorityqueue.java

---
+++ Preorityqueue.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Preorityqueue{
+    public static void main(String... jgd){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(10);
+        pq.add(20);
+        pq.add(10);
+        pq.add(10);
+        pq.add(20);
+        pq.add(10);
+
+    System.out.println(pq);
+
+    }
+} 
